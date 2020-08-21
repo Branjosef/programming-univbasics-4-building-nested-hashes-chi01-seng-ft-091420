0e05719e0722351a8c9781e159fe1749d515c6ae
Brandon Fonnemann <branjosef@gmail.com>
2020-08-21 13:30:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,14 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  base_hash = {:railroads => {:pieces => "4", :rent_in_dollars => { } }}
+  base_hash = {
+    :railroads => {
+      :pieces => "4", 
+      :rent_in_dollars => { },
+      :names => { }
+      }
+  }
   return base_hash
-  
 end
 
 def monopoly_with_third_tier
